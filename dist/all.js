bce005f2156a9f810cb8953eee756285698eaacb
madew <antosmich@gmail.com>
2022-06-15 21:35:51 +0200

    Added display counter in popup section

--- dist/all.js
+++ dist/all.js
@@ -1,26 +1,42 @@
 // DOM Selector
 const btn = document.getElementById('popup_btn');
-const popup = document.querySelector('.popup');
+const popupEl = document.querySelector('.popup');
 const closePopupBtn = document.querySelector('.popup__close-icon');
+const counterEl = document.getElementById('counter')
 
+
+// Counter
+let counter = 0;
+
+
+// Functions
 function openPopup() {
-    popup.classList.add('popup__toggle');
+    popupEl.classList.add('popup__toggle');
 }
 
 function closePopup() {
-    popup.classList.remove('popup__toggle');
+    popupEl.classList.remove('popup__toggle');
 }
 
+
+
+// Listeners
+document.addEventListener('click', event => {
+    counter++;
+    console.log(counter);
+    counterEl.textContent = counter;
+
+})
 
 btn.addEventListener('click', ()=> {
     openPopup();
 })
 
 closePopupBtn.addEventListener('click', ()=> {
-    closePopup()
+    closePopup();
 })
 
-popup.addEventListener('click', (event) => {
-    const isOutside = !event.target.closest('.popup__content')
-    isOutside ? closePopup() : null
+popupEl.addEventListener('click', (event) => {
+    const isOutside = !event.target.closest('.popup__content');
+    isOutside ? closePopup() : null;
 })
