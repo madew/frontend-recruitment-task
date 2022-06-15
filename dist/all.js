ae87616f334c8cd9f38a93d6c8e1c727285f092b
madew <antosmich@gmail.com>
2022-06-15 22:06:27 +0200

    Saving counter to local storage
    q

--- dist/all.js
+++ dist/all.js
@@ -2,11 +2,11 @@
 const btn = document.getElementById('popup_btn');
 const popupEl = document.querySelector('.popup');
 const closePopupBtn = document.querySelector('.popup__close-icon');
-const counterEl = document.getElementById('counter')
+const counterEl = document.getElementById('counter');
 
 
 // Counter
-let counter = 0;
+let counter = loadFromLocalStorage('counter');
 
 
 // Functions
@@ -18,14 +18,22 @@ function closePopup() {
     popupEl.classList.remove('popup__toggle');
 }
 
+function saveToLocalStorage(key, item) {
+    localStorage.setItem(key, JSON.stringify(item))
+}
+
+function loadFromLocalStorage(key) {
+   return JSON.parse(localStorage.getItem(key))
+}
+
+
 
 
 // Listeners
-document.addEventListener('click', event => {
+document.addEventListener('click', ()=> {
     counter++;
-    console.log(counter);
+    saveToLocalStorage('counter', counter)
     counterEl.textContent = counter;
-
 })
 
 btn.addEventListener('click', ()=> {
@@ -36,7 +44,7 @@ closePopupBtn.addEventListener('click', ()=> {
     closePopup();
 })
 
-popupEl.addEventListener('click', (event) => {
+popupEl.addEventListener('click', (event)=> {
     const isOutside = !event.target.closest('.popup__content');
     isOutside ? closePopup() : null;
 })
